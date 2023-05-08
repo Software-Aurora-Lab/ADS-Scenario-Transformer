19694de9b41de58e51f5680f4788cc1732e901b6
Changnam Hong <changnah@sora3.ics.uci.edu>
2023-05-08 14:19:44 -0700

    first commit

---
+++ osm_to_lanelet2.py
@@ -0,0 +1,11 @@
+import lanelet2
+
+dir = "4552b7ac-4fba-4420-88b8-ae14289bf5ac"
+filename = "BorregasAve"
+origin = lanelet2.io.Origin(37.400,-122.023)
+
+map = lanelet2.io.load(f"./{dir}/{filename}.osm", origin)
+
+outputfile = f"{filename}.bin"
+lanelet2.io.write(outputfile, map, origin)
+
