c0189f87c7298d128659272e896e5b6dfbba4f07
Changnam Hong <hcn1519@gmail.com>
2023-05-09 15:32:48 -0700

    Adopt UTMProjector

--- osm_to_lanelet2.py
+++ osm_to_lanelet2.py
@@ -1,11 +1,19 @@
 import lanelet2
+from lanelet2.projection import (UtmProjector, MercatorProjector,
+                                 LocalCartesianProjector, GeocentricProjector)
 
 dir = "4552b7ac-4fba-4420-88b8-ae14289bf5ac"
 filename = "BorregasAve"
-origin = lanelet2.io.Origin(37.400,-122.023)
+origin = lanelet2.io.Origin(37.4168652058464,-122.015518188366)
 
-map = lanelet2.io.load(f"./{dir}/{filename}.osm", origin)
+projector = UtmProjector(origin)
+
+# map = lanelet2.io.load(f"./{dir}/{filename}.osm", origin)
+map = lanelet2.io.load(f"./{dir}/{filename}.osm", projector)
 
 outputfile = f"{filename}-lanelet2.osm"
-lanelet2.io.write(outputfile, map, origin)
+write_errors = lanelet2.io.writeRobust(outputfile, map, projector)
+print(write_errors)
+
+# lanelet2.io.write(outputfile, map, origin)
 
