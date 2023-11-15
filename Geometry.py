d6c9aaaa6351cf3b1d9469e2ab0685f3e76f47f7
Changnam Hong <hcn1519@gmail.com>
2023-11-14 19:40:36 -0800

    Add pyproj, add functions for converting GPSPoint to LanePosition

--- Geometry.py
+++ Geometry.py
@@ -4,12 +4,18 @@ import lanelet2
 from lanelet2.projection import UtmProjector
 from lanelet2.core import (Lanelet, LaneletMap, GPSPoint, BasicPoint2d, BasicPoint3d, getId, Point2d, Point3d)
 from lanelet2.io import Origin
-from lanelet2.geometry import (findNearest, distanceToCenterline2d, distanceToCenterline3d, distance,
-                               findWithin3d)
+from lanelet2.geometry import (findNearest, distanceToCenterline2d, distance, findWithin3d)
+from pyproj import Proj, transform
 
+class UTMPose:
+    def __init__(self, x: float, y: float, zone: int):
+        self.x = x
+        self.y = y
+        self.zone = 10
 
 class Geometry:
-    def find_lanelet(self, map: LaneletMap, basic_point: BasicPoint3d) -> Optional[Lanelet]:
+    @staticmethod
+    def find_lanelet(map: LaneletMap, basic_point: BasicPoint3d) -> Optional[Lanelet]:
         found_lane = findWithin3d(map.laneletLayer, basic_point, 0)[0]
 
         if found_lane:
@@ -18,7 +24,8 @@ class Geometry:
         basic_point2d = BasicPoint2d(basic_point.x, basic_point.y)
         return findNearest(map.laneletLayer, basic_point2d, 1)[0][1]
 
-    def lane_position(self, lanelet: Lanelet, basic_point: BasicPoint3d):
+    @staticmethod
+    def lane_position(lanelet: Lanelet, basic_point: BasicPoint3d):
         point3d = Point3d(getId(), basic_point.x, basic_point.y, basic_point.z)
         s_attribute = distance(lanelet.leftBound[0], point3d)
 
@@ -26,3 +33,13 @@ class Geometry:
         t_attribute = distanceToCenterline2d(lanelet, basic_point2d)
 
         return {"laneId": lanelet.id, "s": s_attribute, "offset": t_attribute}
+    @staticmethod
+    def utm_to_WGS(pose: UTMPose):
+        utm_proj = Proj(proj="utm", zone=pose.zone, ellps="WGS84")
+        lon, lat = utm_proj(pose.x, pose.y, inverse=True)
+        return GPSPoint(lat=lat, lon=lon, ele=0)
+
+    @staticmethod
+    def project_UTM_to_lanelet(projector: UtmProjector, pose: UTMPose):
+        gps_point = Geometry.utm_to_WGS(pose)
+        return projector.forward(gps_point)
--- testGeometry.py
+++ testGeometry.py
@@ -8,13 +8,14 @@ from lanelet2.io import Origin
 from lanelet2.geometry import (findNearest, distanceToCenterline2d, distanceToCenterline3d, distance,
                                findWithin3d)
 
-from Geometry import Geometry
+from Geometry import Geometry, UTMPose
 
 
 class TestGeometry(unittest.TestCase):
     def setUp(self):
         origin = Origin(37.04622247590861, -123.00000000000001, 0)
-        self.map = lanelet2.io.load("./samples/map/BorregasAve/lanelet2_map.osm", UtmProjector(origin))
+        self.utm_projector = UtmProjector(origin)
+        self.map = lanelet2.io.load("./samples/map/BorregasAve/lanelet2_map.osm", self.utm_projector)
 
     def test_load_map(self):
         self.assertIsNotNone(self.map, "Map should not be None")
@@ -22,24 +23,39 @@ class TestGeometry(unittest.TestCase):
     def prop(self, obj):
         return str(type(obj)) + ' ' + str(list(filter(lambda name: not name.startswith('__'), dir(obj))))
 
+    def test_gps(self):
+        poses = [UTMPose(x=587079.3045861976, y=4141574.299574421, zone=10),
+                 UTMPose(x=587044.4300003723, y=4141550.060588833, zone=10)]
+
+        expectations = [BasicPoint3d(87079.3, 41574.3, 0),
+                        BasicPoint3d(87044.4, 41550.1, 0)]
+
+        for pose, expectation in zip(poses, expectations):
+            projected = Geometry.project_UTM_to_lanelet(projector=self.utm_projector, pose=pose)
+            self.assertAlmostEqual(abs(projected.x - expectation.x), second=1.0,
+                                   msg="projected point.x is not equal to the expectation", delta=1.0)
+            self.assertAlmostEqual(abs(projected.y - expectation.y), second=1.0,
+                                   msg="projected point.y is not equal to the expectation", delta=1.0)
+
     def test_geometry(self):
         basic_points = [BasicPoint3d(86973.4293, 41269.817, -5.6757),
                         BasicPoint3d(86993.2289, 41343.5182, -4.5032),
-                        BasicPoint3d(87014.2461, 41427.1901, -3.2535)]
+                        BasicPoint3d(87014.2461, 41427.1901, -3.2535),
+                        BasicPoint3d(87079.3, 41574.3, 0)]
 
         expectations = [
             {"laneId":154, "s": 10.9835, "offset": -0.5042},
             {"laneId":108, "s": 35.266, "offset": -1.1844},
-            {"laneId":108, "s": 121.5308, "offset": -0.134}
+            {"laneId":108, "s": 121.5308, "offset": -0.134},
+            {"laneId": 22, "s": 35.7761, "offset": -0.2818}
         ]
 
-        geometry = Geometry()
         for idx, (basic_point, expectation) in enumerate(zip(basic_points, expectations)):
-            geometry.find_lanelet(self.map, basic_point)
-            lanelet = geometry.find_lanelet(map=self.map, basic_point=basic_point)
+            Geometry.find_lanelet(self.map, basic_point)
+            lanelet = Geometry.find_lanelet(map=self.map, basic_point=basic_point)
             self.assertIsNotNone(lanelet, "lanelet should not be None")
 
-            target_lane_position = geometry.lane_position(lanelet, basic_point)
+            target_lane_position = Geometry.lane_position(lanelet, basic_point)
             print("Lane Position", target_lane_position)
 
             self.assertEqual(target_lane_position["laneId"], expectation["laneId"], "laneId should be the same")
