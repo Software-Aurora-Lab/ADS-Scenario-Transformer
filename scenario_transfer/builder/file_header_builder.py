4caa234f9cdf8166c1a408e2cb389fc45196f334
Changnam Hong <hcn1519@gmail.com>
2024-02-27 23:41:46 +0000

    [#9] Implement FileHeaderBuilder
    - Define default FileHeader output
    - Write test cases

--- scenario_transfer/builder/file_header_builder.py
+++ scenario_transfer/builder/file_header_builder.py
@@ -1,11 +1,33 @@
+from datetime import datetime
+from typing import Dict, Optional
 from openscenario_msgs import FileHeader
 from scenario_transfer.builder import Builder
 
 
 class FileHeaderBuilder(Builder):
+    """
+    message FileHeader {
+        required uint32 revMajor = 1;                   // 1..1
+        required uint32 revMinor = 2;                   // 1..1
+        required string date = 3;                       // 1..1, YYYY-MM-DDThh:mm:ss
+        required string description = 4;                // 1..1
+        required string author = 5;                     // 1..1
+    }
+    """
 
-    def __init__(self):
-        pass
+    product: FileHeader
+
+    def __init__(self, dict: Optional[Dict] = None):
+
+        if dict:
+            self.product = FileHeader(**dict)
+        else:
+            self.product = FileHeader(
+                revMajor=1,
+                revMinor=1,
+                date=datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
+                description="Default FileHeader",
+                author="ADS Scenario Tranferer")
 
     def get_result(self) -> FileHeader:
-        return FileHeader()
+        return self.product
