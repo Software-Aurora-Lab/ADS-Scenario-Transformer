38bc973295aab7a1b5a4e5a8a88ad1409d0780d2
Changnam Hong <hcn1519@gmail.com>
2024-02-27 23:41:46 +0000

    [#9] Implement FileHeaderBuilder
    - Define default FileHeader output
    - Write test cases

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,14 +1,27 @@
 import unittest
-
-from openscenario_msgs import CatalogDefinition
-from scenario_transfer.builder import CatalogDefinitionBuilder
+from datetime import datetime
+from openscenario_msgs import CatalogDefinition, FileHeader
+from scenario_transfer.builder import CatalogDefinitionBuilder, FileHeaderBuilder
 
 
 class TestBuilder(unittest.TestCase):
 
+    def test_file_header_builder(self):
+        builder = FileHeaderBuilder()
+        file_header = builder.get_result()
+        self.assertIsInstance(file_header, FileHeader)
+
+        expected_format = '%Y-%m-%dT%H:%M:%S'
+        try:
+            datetime.strptime(file_header.date, expected_format)
+        except ValueError:
+            self.fail(f"Date format should be {expected_format}")
+        self.assertEqual(file_header.description, "Default FileHeader")
+        self.assertEqual(file_header.author, "ADS Scenario Tranferer")
+
     def test_catalog_definition_builder(self):
-        catalog_definition_builder = CatalogDefinitionBuilder()
-        catalog_definition = catalog_definition_builder.get_result()
+        builder = CatalogDefinitionBuilder()
+        catalog_definition = builder.get_result()
         self.assertIsInstance(catalog_definition, CatalogDefinition)
 
 
