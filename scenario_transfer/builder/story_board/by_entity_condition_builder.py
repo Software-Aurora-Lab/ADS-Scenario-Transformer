417d846e72079e57b53b6ff286c08b18f3243551
Changnam Hong <hcn1519@gmail.com>
2024-03-19 23:38:18 +0000

    [#12] Implement ByEntityConditionBuilder
    - CollisionCondition
    - AccelerationCondition

---
+++ scenario_transfer/builder/story_board/by_entity_condition_builder.py
@@ -0,0 +1,86 @@
+from typing import List
+
+from openscenario_msgs import Condition, TriggeringEntities
+import openscenario_msgs.common_pb2 as common_pb2
+from openscenario_msgs.common_pb2 import ByEntityCondition, EntityCondition, EndOfRoadCondition, CollisionCondition, OffroadCondition, TimeHeadwayCondition, TimeToCollisionCondition, AccelerationCondition, StandStillCondition, SpeedCondition, RelativeSpeedCondition, TraveledDistanceCondition, DistanceCondition, RelativeDistanceCondition, RelativeClearanceCondition
+from scenario_transfer.builder import Builder
+from openscenario_msgs import Rule, EntityRef
+
+
+class ByEntityConditionBuilder(Builder):
+    product: ByEntityCondition
+    entity_condition: EntityCondition
+    triggering_entities: TriggeringEntities
+
+    def __init__(self, triggering_entity: str):
+
+        self.triggering_entities = TriggeringEntities(
+            triggeringEntitiesRule=common_pb2.TriggeringEntities.
+            TriggeringEntitiesRule.ANY,
+            entityRef=[EntityRef(entityRef=triggering_entity)
+                       ])  # limit to use single entity
+
+    def make_collision_condition(self, colliding_entity_name: str):
+        condition = CollisionCondition(entityRef=EntityRef(
+            entityRef=colliding_entity_name))
+        self.entity_condition = EntityCondition(collisionCondition=condition)
+
+    def make_time_headway_condition(self):
+        # Implement the logic for time_headway_condition
+        return TimeHeadwayCondition()
+
+    def make_acceleration_condition(self, value_in_ms: float, rule: Rule):
+        condition = AccelerationCondition(value=value_in_ms, rule=rule)
+        self.entity_condition = EntityCondition(
+            accelerationCondition=condition)
+
+    def make_stand_still_condition(self):
+        # Implement the logic for stand_still_condition
+        return StandStillCondition()
+
+    def make_speed_condition(self):
+        # Implement the logic for speed_condition
+        return SpeedCondition()
+
+    def make_distance_condition(self):
+        # Implement the logic for distance_condition
+        return DistanceCondition()
+
+    def make_relative_distance_condition(self):
+        # Implement the logic for relative_distance_condition
+        return RelativeDistanceCondition()
+
+    def make_relative_clearance_condition(self):
+        # Implement the logic for relative_clearance_condition
+        return RelativeClearanceCondition()
+
+    def get_result(self) -> ByEntityCondition:
+        assert self.entity_condition is not None
+        assert self.triggering_entities is not None
+
+        self.product = ByEntityCondition(
+            triggeringEntities=self.triggering_entities,
+            entityCondition=self.entity_condition)
+        return self.product
+
+    # Unsupported condition types.
+
+    def make_end_of_road_condition(self):
+        raise TypeError(
+            f"Unsupported type: {type(EndOfRoadCondition()).__name__}")
+
+    def make_offroad_condition(self):
+        raise TypeError(
+            f"Unsupported type: {type(OffroadCondition()).__name__}")
+
+    def make_time_to_collision_condition(self):
+        raise TypeError(
+            f"Unsupported type: {type(TimeToCollisionCondition()).__name__}")
+
+    def make_relative_speed_condition(self):
+        raise TypeError(
+            f"Unsupported type: {type(RelativeSpeedCondition()).__name__}")
+
+    def make_traveled_distance_condition(self):
+        raise TypeError(
+            f"Unsupported type: {type(TraveledDistanceCondition()).__name__}")
---
+++ tests/test_condition_builder.py
@@ -0,0 +1,58 @@
+import unittest
+import yaml
+from openscenario_msgs import Actors, Condition, ByEntityCondition, Rule
+from scenario_transfer.builder.entities_builder import EntityType, EntitiesBuilder
+from scenario_transfer.builder.story_board.by_entity_condition_builder import ByEntityConditionBuilder
+
+
+class TestBuilder(unittest.TestCase):
+
+    def setUp(self):
+        input_dir = "./tests/data/"
+        self.route_file_path = input_dir + "openscenario_route.yaml"
+        builder = EntitiesBuilder(entities=[
+            EntityType.NPC, EntityType.NPC, EntityType.EGO,
+            EntityType.PEDESTRIAN, EntityType.NPC
+        ])
+        self.entities = builder.get_result()
+
+    def test_entity_condition_builder_collision(self):
+
+        ego_name = self.entities.scenarioObjects[0].name
+        colliding_npc_name = self.entities.scenarioObjects[1].name
+
+        builder = ByEntityConditionBuilder(triggering_entity=ego_name)
+        builder.make_collision_condition(
+            colliding_entity_name=colliding_npc_name)
+        by_entity_condition = builder.get_result()
+
+        assert by_entity_condition is not None
+        assert by_entity_condition.triggeringEntities.entityRef[
+            0].entityRef == "ego"
+        assert by_entity_condition.entityCondition.collisionCondition is not None
+        assert by_entity_condition.entityCondition.collisionCondition.entityRef.entityRef == "npc_1"
+
+    def test_entity_condition_builder_acceleration(self):
+
+        ego_name = self.entities.scenarioObjects[0].name
+
+        builder = ByEntityConditionBuilder(triggering_entity=ego_name)
+        builder.make_acceleration_condition(value_in_ms=10,
+                                            rule=Rule.GREATER_THAN)
+        by_entity_condition = builder.get_result()
+
+        assert by_entity_condition is not None
+        assert by_entity_condition.triggeringEntities.entityRef[
+            0].entityRef == "ego"
+        assert by_entity_condition.entityCondition.accelerationCondition.value == 10.0
+        assert by_entity_condition.entityCondition.accelerationCondition.rule == Rule.GREATER_THAN
+
+    # def test_condition_builder(self):
+    #     builder = ConditionBuilder()
+    #     ego_name = self.entities.scenarioObjects[0].name
+    #     builder.make_entity_condition(entity_names=[ego_name])
+    #     entity_condition = builder.get_result()
+
+    #     self.assertIsInstance(entity_condition, Condition)
+    #     self.assertIsInstance(entity_condition.byEntityCondition,
+    #                           ByEntityCondition)
