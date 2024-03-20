f7a91dc4dd8a05ba2933b04f6630b246a7dff8e2
Changnam Hong <hcn1519@gmail.com>
2024-03-20 22:50:15 +0000

    [#28] Support SimulationTimeCondition and StoryboardElementStateCondition

--- scenario_transfer/builder/story_board/by_value_condition_builder.py
+++ scenario_transfer/builder/story_board/by_value_condition_builder.py
@@ -1,9 +1,9 @@
 from typing import List
 
-from openscenario_msgs import Condition, TriggeringEntities, Rule
+from openscenario_msgs import Rule
 
-from openscenario_msgs.common_pb2 import ByValueCondition, TimeOfDayCondition, VariableCondition
-from openscenario_msgs.parameter_pb2 import ParameterCondition, ParameterType, ParameterDeclaration
+from openscenario_msgs.common_pb2 import ByValueCondition, SimulationTimeCondition, StoryboardElementStateCondition, TimeOfDayCondition, VariableCondition
+from openscenario_msgs.parameter_pb2 import ParameterCondition, ParameterDeclaration
 from scenario_transfer.builder import Builder
 
 
@@ -13,35 +13,49 @@ class ByValueConditionBuilder(Builder):
 
     def __init__(self):
         pass
-    
-    def make_parameter_condition(self, 
-                                 parameter_declaration: ParameterDeclaration, 
-                                 rule: Rule, 
-                                 value: str):
-        self.condition = ParameterCondition(parameterRef=parameter_declaration,rule=rule,value=value)
+
+    def make_parameter_condition(self,
+                                 parameter_declaration: ParameterDeclaration,
+                                 rule: Rule, value: str):
+        self.condition = ParameterCondition(parameterRef=parameter_declaration,
+                                            rule=rule,
+                                            value=value)
         self.product = ByValueCondition(parameterCondition=self.condition)
 
-    def make_simulation_time_condition(self):
-        pass
-    def make_storyboard_element_state_condition(self):
-        pass
+    def make_simulation_time_condition(self, rule: Rule, value_in_sec: float):
+        self.condition = SimulationTimeCondition(rule=rule, value=value_in_sec)
+        self.product = ByValueCondition(simulationTimeCondition=self.condition)
+
+    def make_storyboard_element_state_condition(
+            self, state: StoryboardElementStateCondition.State,
+            element_name: str, type: StoryboardElementStateCondition.Type):
+
+        self.condition = StoryboardElementStateCondition(
+            state=state,
+            storyboardElementRef=element_name,
+            storyboardElementType=type)
+        self.product = ByValueCondition(
+            storyboardElementStateCondition=self.condition)
+
     def make_user_defined_value_condition(self):
         pass
+
     def make_traffic_signal_condition(self):
         pass
+
     def make_traffic_signal_controller_condition(self):
         pass
 
     def get_result(self) -> ByValueCondition:
         assert self.product is not None
         return self.product
-        
+
     # Unsupported condition types
-        
+
     def make_time_of_day_condition(self):
         raise TypeError(
             f"Unsupported type: {type(TimeOfDayCondition()).__name__}")
-        
+
     def make_variable_condition(self):
         raise TypeError(
             f"Unsupported type: {type(VariableCondition()).__name__}")
--- tests/test_condition_builder.py
+++ tests/test_condition_builder.py
@@ -1,6 +1,6 @@
 import unittest
 import yaml
-from openscenario_msgs import Actors, Condition, ByEntityCondition, Rule, LanePosition, Position, DirectionalDimension, RelativeDistanceType, CoordinateSystem, RoutingAlgorithm
+from openscenario_msgs import Actors, Condition, ByEntityCondition, Rule, LanePosition, Position, DirectionalDimension, RelativeDistanceType, CoordinateSystem, RoutingAlgorithm, StoryboardElementStateCondition
 from openscenario_msgs.parameter_pb2 import ParameterDeclaration, ParameterType
 from scenario_transfer.builder.entities_builder import EntityType, EntitiesBuilder
 from scenario_transfer.builder.story_board.by_entity_condition_builder import ByEntityConditionBuilder
@@ -18,6 +18,8 @@ class TestConditionBuilder(unittest.TestCase):
         ])
         self.entities = builder.get_result()
         self.ego_name = self.entities.scenarioObjects[0].name
+
+    # Entity Condition
 
     def test_entity_condition_builder_collision(self):
 
@@ -51,8 +53,8 @@ class TestConditionBuilder(unittest.TestCase):
 
         builder = ByEntityConditionBuilder(triggering_entity=self.ego_name)
         builder.make_speed_condition(
-            direction=DirectionalDimension.DIRECTIONALDIMENSION_LONGITUDINAL, 
-            value_in_ms=0.001, 
+            direction=DirectionalDimension.DIRECTIONALDIMENSION_LONGITUDINAL,
+            value_in_ms=0.001,
             rule=Rule.GREATER_THAN)
         by_entity_condition = builder.get_result()
 
@@ -90,24 +92,14 @@ class TestConditionBuilder(unittest.TestCase):
             0].entityRef == "ego"
         assert by_entity_condition.entityCondition.reachPositionCondition.position.lanePosition.laneId == "154"
 
-    # def make_distance_condition(self, 
-    #     coordinateSystem: CoordinateSystem,
-    #     freespace: bool,
-    #     relativeDistanceType: RelativeDistanceType,
-    #     routingAlgorithm: RoutingAlgorithm,
-    #     value_in_meter: float,
-    #     rule: Rule,
-    #     position: Position):
-        
     def test_entity_condition_builder_distance(self):
-
         lane_position = LanePosition(laneId="154", s=10.9835, offset=-0.5042)
 
         builder = ByEntityConditionBuilder(triggering_entity=self.ego_name)
         builder.make_distance_condition(
             coordinateSystem=CoordinateSystem.LANE,
             freespace=True,
-            relativeDistanceType = RelativeDistanceType.
+            relativeDistanceType=RelativeDistanceType.
             RELATIVEDISTANCETYPE_LATERAL,
             routingAlgorithm=RoutingAlgorithm.SHORTEST,
             value_in_meter=5,
@@ -129,7 +121,7 @@ class TestConditionBuilder(unittest.TestCase):
         builder.make_time_headway_condition(
             coordinateSystem=CoordinateSystem.LANE,
             entity_name=headway_npc_name,
-            relativeDistanceType = RelativeDistanceType.
+            relativeDistanceType=RelativeDistanceType.
             RELATIVEDISTANCETYPE_LATERAL,
             rule=Rule.GREATER_THAN,
             value_in_sec=3)
@@ -161,12 +153,18 @@ class TestConditionBuilder(unittest.TestCase):
         assert by_entity_condition.entityCondition.relativeDistanceCondition.value == 5
         assert by_entity_condition.entityCondition.relativeDistanceCondition.entityRef == target_npc_name
 
+    # Value Condition
+
     def test_value_condition_builder_parameter(self):
-        
+
         builder = ByValueConditionBuilder()
         builder.make_parameter_condition(
-            parameter_declaration=ParameterDeclaration(name="param", parameterType=ParameterType.PARAMETERTYPE_INT, value="1", constraintGroups=[]), 
-            rule=Rule.GREATER_THAN, 
+            parameter_declaration=ParameterDeclaration(
+                name="param",
+                parameterType=ParameterType.PARAMETERTYPE_INT,
+                value="1",
+                constraintGroups=[]),
+            rule=Rule.GREATER_THAN,
             value="condition_value")
 
         by_value_condition = builder.get_result()
@@ -175,3 +173,28 @@ class TestConditionBuilder(unittest.TestCase):
         declaration = by_value_condition.parameterCondition.parameterRef
         assert declaration.name == "param"
         assert declaration.parameterType == ParameterType.PARAMETERTYPE_INT
+
+    def test_value_condition_builder_simulation_time(self):
+
+        builder = ByValueConditionBuilder()
+        builder.make_simulation_time_condition(rule=Rule.GREATER_THAN,
+                                               value_in_sec=180)
+
+        by_value_condition = builder.get_result()
+        assert by_value_condition is not None
+        assert by_value_condition.simulationTimeCondition.rule == Rule.GREATER_THAN
+        assert by_value_condition.simulationTimeCondition.value == 180
+
+    def test_value_condition_builder_storyboard_element_state(self):
+
+        builder = ByValueConditionBuilder()
+        builder.make_storyboard_element_state_condition(
+            state=StoryboardElementStateCondition.State.COMPLETE_STATE,
+            element_name="act_ego_testspeed_check",
+            type=StoryboardElementStateCondition.Type.MANEUVER)
+
+        by_value_condition = builder.get_result()
+        assert by_value_condition is not None
+        assert by_value_condition.storyboardElementStateCondition.state == StoryboardElementStateCondition.State.COMPLETE_STATE
+        assert by_value_condition.storyboardElementStateCondition.storyboardElementRef == "act_ego_testspeed_check"
+        assert by_value_condition.storyboardElementStateCondition.storyboardElementType == StoryboardElementStateCondition.Type.MANEUVER
