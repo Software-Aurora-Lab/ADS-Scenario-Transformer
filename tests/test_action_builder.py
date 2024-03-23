a809b54ebd21b8a80fb1fb767b670ce3fa755510
changnam hong <hcn1519@gmail.com>
2024-03-23 14:04:32 -0700

    [#29] Implement builder for TeleportAction and AssignControllerAction

--- tests/test_action_builder.py
+++ tests/test_action_builder.py
@@ -1,11 +1,10 @@
 import pytest
-from openscenario_msgs import GlobalAction, Entities, Position, LanePosition, TransitionDynamics, AbsoluteTargetSpeed, RelativeTargetSpeed, FollowingMode
+from openscenario_msgs import GlobalAction, Entities, Position, LanePosition, WorldPosition, TransitionDynamics, AbsoluteTargetSpeed, RelativeTargetSpeed, FollowingMode, Properties, Property, Controller, ControllerAction, AssignControllerAction, TeleportAction
 from openscenario_msgs.common_pb2 import InfrastructureAction, EntityAction, LaneChangeAction, UserDefinedAction, PrivateAction, SpeedTargetValueType, SpeedAction
 from scenario_transfer.builder.story_board.global_action_builder import GlobalActionBuilder
 from scenario_transfer.builder.story_board.user_defined_action_builder import UserDefinedActionBuilder
 from scenario_transfer.builder.story_board.private_action_builder import PrivateActionBuilder
 from scenario_transfer.builder.entities_builder import EntityType, EntitiesBuilder
-
 
 @pytest.fixture
 def entities() -> Entities:
@@ -29,6 +28,26 @@ def transition_dynamics() -> TransitionDynamics:
         followingMode=FollowingMode.FOLLOWINGMODE_FOLLOW,
         value=1.0)
 
+@pytest.fixture
+def properties() -> Properties:
+    return Properties(properties=[
+        Property(name="isEgo", value="true"),
+        Property(name="maxSpeed", value="50")
+    ])
+
+@pytest.fixture
+def controller(properties) -> Controller:
+    return Controller(name="controller",
+                      properties=properties,
+                      parameterDeclarations=[])
+
+@pytest.fixture
+def lane_position() -> LanePosition:
+    return LanePosition(laneId="154", s=10.9835, offset=-0.5042)
+
+@pytest.fixture
+def world_position() -> WorldPosition:
+    return WorldPosition(x=37.416880423172465, y=-122.01593194093681, z=0.0)
 
 def assert_proto_type_equal(reflection_type, pb2_type):
     assert str(reflection_type.__class__) == str(pb2_type)
@@ -159,3 +178,40 @@ def test_absolute_lane_change_action(transition_dynamics):
     assert_proto_type_equal(
         action.lateralAction.laneChangeAction.laneChangeActionDynamics,
         TransitionDynamics)
+
+def test_assigned_control_action(controller):
+    builder = PrivateActionBuilder()
+    builder.make_assign_controller_action(
+            controller= controller,
+            activate_lateral=True)
+    action = builder.get_result()
+
+    assert action is not None
+    assert_proto_type_equal(action.controllerAction, ControllerAction)
+    assert_proto_type_equal(action.controllerAction.assignControllerAction, AssignControllerAction)
+    target_controller = action.controllerAction.assignControllerAction.controller
+    assert target_controller.properties.properties[0].name == "isEgo"
+    assert target_controller.properties.properties[1].name == "maxSpeed"
+
+def test_teleport_action(world_position, lane_position):
+    builder = PrivateActionBuilder()
+    builder.make_teleport_action(lane_position=lane_position)
+    action = builder.get_result()
+
+    assert action is not None
+    assert_proto_type_equal(action.teleportAction, TeleportAction)
+    lane_position_in_teleport_action = action.teleportAction.position.lanePosition
+    assert_proto_type_equal(lane_position_in_teleport_action, LanePosition)
+    assert lane_position_in_teleport_action.laneId == "154"
+    assert lane_position_in_teleport_action.s == 10.9835
+    assert lane_position_in_teleport_action.offset == -0.5042
+
+    builder.make_teleport_action(world_position=world_position)
+    action = builder.get_result()
+    assert action is not None
+    assert_proto_type_equal(action.teleportAction, TeleportAction)
+    world_position_in_teleport_action = action.teleportAction.position.worldPosition
+    assert_proto_type_equal(world_position_in_teleport_action, WorldPosition)
+    assert world_position_in_teleport_action.x == 37.416880423172465
+    assert world_position_in_teleport_action.y == -122.01593194093681
+    assert world_position_in_teleport_action.z == 0.0
