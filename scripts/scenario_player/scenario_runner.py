c76973cd07a7e38cc8684f8722fd533998ee0e13
Changnam Hong <hcn1519@gmail.com>
2024-05-05 07:55:22 +0000

    [#36] create summary after finish running scenarios

--- scripts/scenario_player/scenario_runner.py
+++ scripts/scenario_player/scenario_runner.py
@@ -1,13 +1,62 @@
 import os
 import glob
 import docker
+import random
+import csv
+from typing import List, Optional
+from dataclasses import dataclass
+import xml.etree.ElementTree as ET
 from container_manager import ContainerManager
+
+
+@dataclass
+class CSVResult:
+    scenario_name: str
+    is_success: bool
+    error_type: Optional[str]
+    message: Optional[str]
+
+
+def create_result_data(result_file_path) -> List[CSVResult]:
+    tree = ET.parse(result_file_path)
+    root = tree.getroot()
+    results = []
+    for testsuite in root.findall('testsuite'):
+        for testcase in testsuite.findall('testcase'):
+            error = testcase.find('error')
+            if error is not None:
+                results.append(
+                    CSVResult(scenario_name=testcase.attrib['name'],
+                              is_success=False,
+                              error_type=error.attrib['type'],
+                              message=error.attrib['message']))
+            else:
+                results.append(
+                    CSVResult(scenario_name=testcase.attrib['name'],
+                              is_success=True,
+                              error_type=None,
+                              message=None))
+    return results
+
+
+def write_result_to_csv(result, filename):
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            ['Scenario Name', 'P/F', 'Error Type', 'Error Message'])
+
+        for result in result:
+            writer.writerow([
+                result.scenario_name, result.is_success, result.error_type,
+                result.message
+            ])
+
 
 if __name__ == '__main__':
     ADS_ROOT = "/home/sora/Desktop/changnam"
     EXP_ROOT = "/home/sora/Desktop/changnam/experiments"
     MAP_PATH = ADS_ROOT + "/autoware_map"
-    DOCKER_CONTAINER_NAME = "transformer"
+    DOCKER_CONTAINER_NAME = "ads_scenario_transformer"
     DOCKER_IMAGE_ID = "6f0050135292"
     EXP_NO = 1
 
@@ -25,7 +74,9 @@ if __name__ == '__main__':
     scenario_paths = glob.glob(pattern, recursive=True)
 
     print("scenario_paths:", scenario_paths)
-    container_id = 45163
+    container_id = random.randint(100, 100000)
+
+    csv_results = []
     for scenario_path in scenario_paths:
 
         container_manager.start_instance(
@@ -34,6 +85,7 @@ if __name__ == '__main__':
             map_path=MAP_PATH,
             scenario_path=SCENARIO_DIR,
             script_path=SCRIPT_DIR,
+            log_path=LOG_DIR,
             docker_image_id=DOCKER_IMAGE_ID)
 
         running_script_path = container_manager.create_scenario_running_script(
@@ -47,3 +99,10 @@ if __name__ == '__main__':
             container_manager.execute_script_in_container(running_script_path))
         container_manager.remove_instance()
         container_id += 1
+
+        results = create_result_data(result_file_path=LOG_DIR +
+                                     "/scenario_test_runner/result.junit.xml")
+        csv_results.extend(results)
+
+    write_result_to_csv(result=csv_results,
+                        filename=SINGLE_EXP_ROOT + "summary.csv")
