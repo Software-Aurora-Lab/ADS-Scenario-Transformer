7952868ba069ba47e03ad6d9466ce14850d2c9df
Changnam Hong <hcn1519@gmail.com>
2024-05-05 06:41:47 +0000

    Update run_transformer script to reuse map parser instance

--- scripts/run_transformer.py
+++ scripts/run_transformer.py
@@ -3,6 +3,9 @@ import os
 import subprocess
 from pathlib import Path
 import json
+import pprint
+from ads_scenario_transformer.transformer.scenario_transformer import ScenarioTransformer, ScenarioTransformerConfiguration
+from ads_scenario_transformer.openscenario.openscenario_coder import OpenScenarioEncoder
 
 
 def file_has_extension(filename, extension):
@@ -18,36 +21,46 @@ def run_scenario_transformer(directory_path, config_path):
     with open(config_path, 'r') as file:
         config = json.load(file)
 
-    for filename in os.listdir(directory_path):
+    for filename in sorted(os.listdir(directory_path)):
         full_file_path = os.path.join(directory_path, filename)
 
         if file_has_extension(full_file_path, "00000"):
-            command = [
-                'poetry', 'run', 'python', '-m',
-                'ads_scenario_transformer.__main__', '--apollo-map-path',
-                config["apollo-map-path"], '--vector-map-path',
-                config["vector-map-path"], '--apollo-scenario-path',
-                full_file_path, '--road-network-lanelet-map-path',
-                config["road-network-lanelet-map-path"], '--source-name',
-                config["source-name"], '--obstacle-waypoint-frequency',
-                str(config["obstacle-waypoint-frequency"]),
-                '--output-scenario-path', config["output-scenario-path"]
-            ]
 
-            if config["disable-traffic-signal"]:
-                command.append('--disable-traffic-signal')
+            configuration = ScenarioTransformerConfiguration(
+                apollo_scenario_path=full_file_path,
+                apollo_hd_map_path=config["apollo-map-path"],
+                vector_map_path=config["vector-map-path"],
+                road_network_lanelet_map_path=config[
+                    "road-network-lanelet-map-path"],
+                road_network_pcd_map_path="point_cloud.pcd",
+                obstacle_direction_change_detection_threshold=0,
+                obstacle_waypoint_frequency_in_sec=config[
+                    "obstacle-waypoint-frequency"],
+                disable_traffic_signal=config["disable-traffic-signal"],
+                use_last_position_as_destination=config[
+                    "use-last-position-destination"])
 
-            if config["use-last-position-destination"]:
-                command.append('--use-last-position-destination')
+            transformer = ScenarioTransformer(configuration=configuration)
+            scenario = transformer.transform()
+            scenario_yaml = OpenScenarioEncoder.encode_proto_pyobject_to_yaml(
+                proto_pyobject=scenario, wrap_result_with_typename=False)
 
-            print("Executing command:", ' '.join(command))
+            filename = ""
+            if config["source-name"]:
+                filename = config["source-name"]
+                filename = filename + "-" + Path(full_file_path).stem
+            else:
+                filename = Path(full_file_path).stem
 
-            # Execute the command
-            result = subprocess.run(command, capture_output=True, text=True)
-            if result.stdout:
-                print("Output:", result.stdout)
-            if result.stderr:
-                print("Error:", result.stderr)
+            output_path = Path(
+                config["output-scenario-path"]) / (filename + ".yaml")
+            output_path.parent.mkdir(parents=True, exist_ok=True)
+
+            print("Configuration")
+            pprint.pprint(vars(configuration), indent=4)
+            print(f"File will saved at {output_path}")
+            with open(output_path, 'w') as file:
+                file.write(scenario_yaml)
 
 
 if __name__ == "__main__":
