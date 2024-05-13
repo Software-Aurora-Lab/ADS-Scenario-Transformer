cf8f6bcbf6199c42dd46951ccfc4c4f8f5b95db6
Changnam Hong <hcn1519@gmail.com>
2024-05-13 21:44:55 +0000

    Update StartTriggerBuilder to make multiple ConditionGroups

--- ads_scenario_transformer/builder/storyboard/trigger_builder.py
+++ ads_scenario_transformer/builder/storyboard/trigger_builder.py
@@ -7,9 +7,10 @@ class StartTriggerBuilder(Builder):
     product: StartTrigger
 
     def make_condition_group(self, conditions: List[Condition]):
+
         self.product = StartTrigger(conditionGroups=[
-            ConditionGroup(conditions=conditions)
-        ])  # It is uncommon to have multiple ConditionGroups.
+            ConditionGroup(conditions=[condition]) for condition in conditions
+        ])
 
     def get_result(self) -> StartTrigger:
         return self.product
@@ -19,9 +20,10 @@ class StopTriggerBuilder(Builder):
     product: StopTrigger
 
     def make_condition_group(self, conditions: List[Condition]):
+
         self.product = StopTrigger(conditionGroups=[
-            ConditionGroup(conditions=conditions)
-        ])  # It is uncommon to have multiple ConditionGroups.
+            ConditionGroup(conditions=[condition]) for condition in conditions
+        ])
 
     def get_result(self) -> StopTrigger:
         return self.product
